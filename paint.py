a567b6483c017fa280d26977f020e3de08889177
rmqlife <rmqlife@gmail.com>
2016-05-04 22:26:26 +0800

    stable version find the contour of the paint frame in test.jpg

--- paint.py
+++ paint.py
@@ -1,3 +1,10 @@
+# reference
+# http://docs.opencv.org/2.4/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html?highlight=findcontours
+# http://www.pyimagesearch.com/2014/04/21/building-pokedex-python-finding-game-boy-screen-step-4-6/
+# http://www.pyimagesearch.com/2014/05/05/building-pokedex-python-opencv-perspective-warping-step-5-6/
+# https://en.wikipedia.org/wiki/Bilateral_filter
+# /opencv/samples/python2/contours.py
+# https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
 import cv2
 def find(img,debug=False):
     while (max(img.shape[:2])>1000):
@@ -19,6 +26,27 @@ def find(img,debug=False):
     if debug:
         cv2. imshow("edge",edge)
     
+    # find contours
+    (_, cnts, _) = cv2.findContours(edge.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # find the top 10 contours
+    cnts = sorted(cnts, key = cv2. contourArea, reverse = True)[:10]
+    
+    # loop over contours
+    for c in cnts:
+        # perimeter
+        peri = cv2.arcLength(c, True)
+        # https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
+        approx = cv2.approxPolyDP(c, 0.02*peri, True)
+        # if the approximated contour has four points, assume it is a rect
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+                
+    # draw contours
+    cv2.drawContours(img, [screenCnt] , -1, (255,0,0), 2)
+    if debug:
+        cv2.imshow("cnts",img)
     if debug:
         cv2.waitKey(0)
     pass
